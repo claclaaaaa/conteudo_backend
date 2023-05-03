bc7d1cd1c1b4da29991cece3c25be0bacc2f7ad2
Clara <clara.hohne@alunos.ifsuldeminas.edu.br>
2023-05-03 10:39:30 -0300

    aula15

--- aula13/conexao.php
+++ aula13/conexao.php
@@ -1,18 +1,21 @@
 <?php
 
 $host = "127.0.0.1";
-$user = "root"; 
+$user = "root";
 $password = "";
 $database = "db_catalogo_3infob";
-$port = 3306; 
+$port = 3306;
 
 //cria uma conexão com o mysql
-$conexao = new mysqli($host, $user, $password, $database, $port);
+$conexao = new mysqli($host, 
+                      $user, 
+                      $password,
+                      $database,
+                      $port);
 
-//verifica se existem erros na conexão
-if($conexao -> connect_error){
-    echo $conexao -> connect_error;
+//verifica se existe erros na conexão                      
+if($conexao->connect_error){
+    echo $conexao->connect_error;
     die();
 }
 
-?> 
--- aula13/form_usuario.php
+++ aula13/form_usuario.php
@@ -1,3 +1,7 @@
+<?php 
+require_once "usuario/consultar_por_id.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,9 +13,11 @@
 <body>
     <h1>Cadastro de usuário</h1>
     <hr>
+
     <form action="usuario/inserir.php" method="post">
+        
         <label>Nome</label><br>
-        <input type="text" name="nome"><br>
+        <input type="text" name="nome" value="<?php echo $usuario->nome;?>" ><br>
 
         <label>Login</label><br>
         <input type="text" name="login"><br>
@@ -19,7 +25,11 @@
         <label>Senha</label><br>
         <input type="password" name="senha"><br>
         <br>
-        <button type="submit">Cadastrar</button>
+        <button type="submit">Inserir</button>
+        
     </form>
+
+
+    
 </body>
 </html>
