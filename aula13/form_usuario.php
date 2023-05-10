cee4124f69aa42f8ede694045395a006eed6310b
Clara <clara.hohne@alunos.ifsuldeminas.edu.br>
2023-05-10 10:39:11 -0300

    aula16

--- aula13/form_usuario.php
+++ aula13/form_usuario.php
@@ -17,10 +17,10 @@ require_once "usuario/consultar_por_id.php";
     <form action="usuario/inserir.php" method="post">
         
         <label>Nome</label><br>
-        <input type="text" name="nome" value="<?php echo $usuario->nome;?>" ><br>
+        <input type="text" name="nome" value="<?php echo $usuario->nome ?? "" ;?>" ><br>
 
         <label>Login</label><br>
-        <input type="text" name="login"><br>
+        <input type="text" name="login" value="<?php echo $usuario->login ?? "" ;?>" ><br>
 
         <label>Senha</label><br>
         <input type="password" name="senha"><br>
