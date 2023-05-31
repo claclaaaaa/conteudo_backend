95dd28b9e70cf73207cc4dd54952353edf23a1b5
Clara <clara.hohne@alunos.ifsuldeminas.edu.br>
2023-05-31 10:43:11 -0300

    aula sei la oq

--- aula13/template/menu_restrito.php
+++ aula13/template/menu_restrito.php
@@ -1,5 +1,3 @@
-<?php require "../noticia/consultar_categorias.php"; ?>
-
 <!-- Menu Principal -->
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
@@ -10,15 +8,25 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-        <?php foreach($categorias as $noticia): ?>
         <li class="nav-item">
-          <a class="nav-link" href="index.php?assunto=<?php echo $noticia->categoria; ?>"><?php echo $noticia->categoria; ?></a>
+          <a class="nav-link" 
+          href="../site/index.php">
+          Site
+          </a>
         </li>
-        <?php endforeach; ?>
 
         <li class="nav-item">
           <a class="nav-link" 
-          href="../noticia/index.php">Acesso Restrito</a>
+          href="../noticia/index.php">
+          Notícias
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" 
+          href="../usuario/index.php">
+          Usuários
+          </a>
         </li>
 
       </ul>
